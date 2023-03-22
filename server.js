6233273603379fbaaa95d2da88319a6627c0b023
Artur Bordignon <arturbordignon6@gmail.com>
2023-03-22 15:28:28 -0300

    Ready to deploy backend

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["https://app.educagenius.com"],
     credentials: true,
   })
 );
